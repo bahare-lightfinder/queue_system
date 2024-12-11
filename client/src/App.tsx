cab614c4fe3981cf225f96d927be341dea160a9a
bahare-lightfinder <123542526+bahare-lightfinder@users.noreply.github.com>
2024-12-11 11:49:40 +0100

    add firebase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   const [backendData, setBackendData] = React.useState([]);
+  const [name, setName] = React.useState("");
+  const [age, setAge] = React.useState<number | undefined>(undefined);
 
-  const fetchData = async ()=> {
-    const res =await fetch("/api");
-    const jsonRes = await res.json();
-    console.log('jsonRes', jsonRes)
+  const fetchData = async () => {
+    // const res =await fetch("/api");
+    // const jsonRes = await res.json();
+    // console.log('jsonRes', jsonRes)
   };
+  const handleSubmit = async () => {};
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     fetchData();
-
-  }, [])
-  console.log('efwdwe')
+  }, []);
+  console.log("efwdwe");
   return (
     <div>
-      hello
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Enter your age:
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => setAge(Number(e.target.value))}
+          />
+        </label>
+        <input type="submit" />
+      </form>
     </div>
   );
-}
+};
 
 export default App;
